aeafa6349850559c6499b6f9cac15bcacd3198fd
Kep Amun <Kep@Wayne.edu>
2013-03-28 00:07:27 -0400

    Removed old lab content and code.
    Replaced grass texture with desert texture.

--- SpeedCanyon/SpeedCanyon/Game1.cs
+++ SpeedCanyon/SpeedCanyon/Game1.cs
@@ -16,7 +16,7 @@ namespace SpeedCanyon
     /// </summary>
     public class Game1 : Game
     {
-        Terrain terrain;
+        Terrain _terrain;
 
         public BoundingSphereRenderer BoundingSphereRenderer { get; private set; }
 
@@ -30,18 +30,7 @@ namespace SpeedCanyon
         VertexBuffer _vb;
         BasicEffect _be;
 
-        VertexPositionColorTexture[] _groundVertices = new VertexPositionColorTexture[4];
-        Texture2D _grassTexture;
-
-
-        Model _baseModel;
-        Model _fanModel;
-        Matrix[] _fanMatrix;
-        Matrix[] _baseMatrix;
-        const int WINDMILL_BASE = 0;
-        const int WINDMILL_FAN = 1;
-        private float _fanRotation = 0.0f;
-        bool _rotatingClockwise = false;
+        Texture2D _desertTexture;
 
         GraphicsDeviceManager _graphics;
         SpriteBatch _spriteBatch;
@@ -71,11 +60,6 @@ namespace SpeedCanyon
 
         // Random
         public Random Rnd { get; protected set; }
-
-        // Shot variables
-        float _shotSpeed = 10;
-        int _shotDelay = 300;
-        int _shotCountdown = 0;
 
         Texture2D _crosshairTexture;
         Texture2D _warningLightTexture;
@@ -155,13 +139,13 @@ namespace SpeedCanyon
 
         private void HandleOffHeightMap(ref int row, ref int col)
         {
-            if (row >= terrain.NUM_ROWS)
-                row = terrain.NUM_ROWS - 1;
+            if (row >= _terrain.NUM_ROWS)
+                row = _terrain.NUM_ROWS - 1;
             else if (row < 0)
                 row = 0;
 
-            if (col >= terrain.NUM_COLS)
-                col = terrain.NUM_COLS - 1;
+            if (col >= _terrain.NUM_COLS)
+                col = _terrain.NUM_COLS - 1;
             else if (col < 0)
                 col = 0;
         }
@@ -170,8 +154,8 @@ namespace SpeedCanyon
         Vector3 RowColumn(Vector3 position)
         {
             // calculate X and Z
-            int col = (int)((position.X + terrain.worldWidth) / terrain.cellWidth);
-            int row = (int)((position.Z + terrain.worldHeight) / terrain.cellHeight);
+            int col = (int)((position.X + _terrain.worldWidth) / _terrain.cellWidth);
+            int row = (int)((position.Z + _terrain.worldHeight) / _terrain.cellHeight);
             HandleOffHeightMap(ref row, ref col);
 
             return new Vector3(col, 0.0f, row);
@@ -181,7 +165,7 @@ namespace SpeedCanyon
         float Height(int row, int col)
         {
             HandleOffHeightMap(ref row, ref col);
-            return terrain.PositionY(col + row * terrain.NUM_COLS);
+            return _terrain.PositionY(col + row * _terrain.NUM_COLS);
         }
 
 
@@ -194,8 +178,8 @@ namespace SpeedCanyon
 
             // distance from top left of cell
             float distanceFromLeft, distanceFromTop;
-            distanceFromLeft = position.X % terrain.cellWidth;
-            distanceFromTop = position.Z % terrain.cellHeight;
+            distanceFromLeft = position.X % _terrain.cellWidth;
+            distanceFromTop = position.Z % _terrain.cellHeight;
 
             // lerp projects height relative to known dimensions
             float topHeight = MathHelper.Lerp(Height(row, col),
@@ -208,123 +192,6 @@ namespace SpeedCanyon
         }
 
 
-        private void InitializeRoutes()
-        {
-            // length of world quadrant
-            const float END = +BOUNDARY; // Error page 346. 
-            // -BOUNDARY should be +BOUNDARY
-
-            // 1st Bezier curve control points (1st route)
-            _bezierA[0] = new Vector3(END + 5.0f, 0.4f, 5.0f);           // start
-            _bezierA[1] = new Vector3(END + 5.0f, 2.4f, 3.0f * END);     // ctrl 1
-            _bezierA[2] = new Vector3(-END - 5.0f, 4.4f, 3.0f * END);    // ctrl 2
-            _bezierA[3] = new Vector3(-END - 5.0f, 5.4f, 5.0f);          // end
-
-            // 1st new Bezier between old Bezier curves (2nd route)
-            _newbezierA[0] = new Vector3(-END - 5.0f, 5.4f, 5.0f);          // start
-            _newbezierA[1] = new Vector3(-END + 20.0f, 2.4f, 3.0f);    // ctrl 1
-            _newbezierA[2] = new Vector3(-END + 20.0f, 4.4f, -3.0f);   // ctrl 2
-            _newbezierA[3] = new Vector3(-END - 5.0f, 5.4f, -5.0f);         // end
-
-            // 2nd Bezier curve control points (3rd route)
-            _bezierB[0] = new Vector3(-END - 5.0f, 5.4f, -5.0f);         // start
-            _bezierB[1] = new Vector3(-END - 5.0f, 4.4f, -3.0f * END);   // ctrl 1
-            _bezierB[2] = new Vector3(END + 5.0f, 2.4f, -3.0f * END);    // ctrl 2
-            _bezierB[3] = new Vector3(END + 5.0f, 0.4f, -5.0f);          // end
-
-            // 2nd new Bezier between old Bezier curves (4th route)
-            _newbezierB[0] = new Vector3(END + 5.0f, 0.4f, -5.0f);         // start
-            _newbezierB[1] = new Vector3(END - 20.0f, 4.4f, -3.0f);   // ctrl 1
-            _newbezierB[2] = new Vector3(END - 20.0f, 2.4f, 3.0f);    // ctrl 2
-            _newbezierB[3] = new Vector3(END + 5.0f, 0.4f, 5.0f);          // end
-        }
-
-        private void InitializeTimeLine()
-        {
-            _keyFrameTime[0] = 4.8f; // time to complete route 1
-            _keyFrameTime[1] = 2.8f; // time to complete route 2
-            _keyFrameTime[2] = 4.8f; // time to complete route 3
-            _keyFrameTime[3] = 2.8f; // time to complete route 4
-        }
-
-        private int KeyFrameNumber()
-        {
-            float timeLapsed = 0.0f;
-
-            // retrieve current leg of trip
-            for (int i = 0; i < NUM_KEYFRAMES; i++)
-            {
-                if (timeLapsed > _tripTime)
-                    return i - 1;
-                else
-                    timeLapsed += _keyFrameTime[i];
-            }
-            return 3;               // special case for last route
-        }
-
-        private Vector3 GetPositionOnCurve(Vector3[] bezier, float fraction)
-        {
-            // returns absolute position on curve based on relative
-            // position on curve (relative position ranges from 0% to 100%)
-            return bezier[0] * (1.0f - fraction) * (1.0f - fraction) * (1.0f - fraction) +
-                    bezier[1] * 3.0f * fraction * (1.0f - fraction) * (1.0f - fraction) +
-                    bezier[2] * 3.0f * fraction * fraction * (1.0f - fraction) +
-                    bezier[3] * fraction * fraction * fraction;
-        }
-
-        private Vector3 GetPositionOnLine(Vector3[] line, float fraction)
-        {
-            // returns absolute position on line based on relative position
-            // on curve (relative position ranges from 0% to 100%)
-            Vector3 lineAtOrigin = line[1] - line[0];
-            return line[0] + fraction * lineAtOrigin;
-        }
-
-        private void UpdateKeyframeAnimation(GameTime gameTime)
-        {
-            // update total trip time, use modulus to prevent variable overflow
-            _tripTime += (gameTime.ElapsedGameTime.Milliseconds / 1000.0f);
-            _tripTime = _tripTime % TOTAL_TRIP_TIME;
-
-            // get the current route number from a total of four routes
-            int routeNum = KeyFrameNumber();
-
-            // sum times for preceding keyframes
-            float keyFrameStartTime = 0.0f;
-
-            for (int i = 0; i < routeNum; i++)
-                keyFrameStartTime += _keyFrameTime[i];
-
-            // calculate time spent during current route
-            float timeBetweenKeys = _tripTime - keyFrameStartTime;
-
-            // calculate percentage of current route completed
-            float fraction = timeBetweenKeys / _keyFrameTime[routeNum];
-
-            // get current X, Y, Z of object being animated
-            // find point on line or curve by passing in % completed
-            switch (routeNum)
-            {
-                case 0: // first curve
-                    _currentPosition = GetPositionOnCurve(_bezierA, fraction);
-                    break;
-                case 1: // first line
-                    _currentPosition = GetPositionOnCurve(_newbezierA, fraction);
-                    break;
-                case 2: // 2nd curve
-                    _currentPosition = GetPositionOnCurve(_bezierB, fraction);
-                    break;
-                case 3: // 2nd line
-                    _currentPosition = GetPositionOnCurve(_newbezierB, fraction);
-                    break;
-            }
-            // get rotation angle about Y based on change in X and Z speed
-            Vector3 speed = _currentPosition - _previousPosition;
-            _previousPosition = _currentPosition;
-            _Yrotation = (float)Math.Atan2((float)speed.X,
-                                                 (float)speed.Z);
-        }
-
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
@@ -333,10 +200,6 @@ namespace SpeedCanyon
         /// </summary>
         protected override void Initialize()
         {
-            InitializeRoutes();
-            InitializeTimeLine();
-
-
             _be = new BasicEffect(GraphicsDevice);
 
             _vb = new VertexBuffer(GraphicsDevice, VertexPositionNormalTexture.VertexDeclaration, 2100, BufferUsage.None);
@@ -351,8 +214,8 @@ namespace SpeedCanyon
                     vertices[35 * z + x].Position.Y = 0;
                     vertices[35 * z + x].Position.Z = 50 * (z - 30);
 
-                    vertices[35 * z + x].TextureCoordinate.X = 10f * x;
-                    vertices[35 * z + x].TextureCoordinate.Y = 10f * z;
+                    vertices[35 * z + x].TextureCoordinate.X = 1f * x;
+                    vertices[35 * z + x].TextureCoordinate.Y = 1f * z;
 
                     vertices[35 * z + x].Normal.X = 0;
                     vertices[35 * z + x].Normal.Y = 1;
@@ -407,23 +270,9 @@ namespace SpeedCanyon
         /// </summary>
         protected override void LoadContent()
         {
-            terrain = Content.Load<Terrain>("Images\\heightMap");
+            _terrain = Content.Load<Terrain>("Images\\heightMap");
 
-            _grassTexture = Content.Load<Texture2D>("Textures\\grass");
-
-            _baseModel = Content.Load<Model>("Models\\base");
-            _baseMatrix = new Matrix[_baseModel.Bones.Count];
-            _baseModel.CopyAbsoluteBoneTransformsTo(_baseMatrix);
-
-            _fanModel = Content.Load<Model>("Models\\fan");
-            _fanMatrix = new Matrix[_fanModel.Bones.Count];
-            _fanModel.CopyAbsoluteBoneTransformsTo(_fanMatrix);
-
-            // load jet
-            _jetModel = Content.Load<Model>("Models\\cf18");
-            _jetMatrix = new Matrix[_jetModel.Bones.Count];
-            _jetModel.CopyAbsoluteBoneTransformsTo(_jetMatrix);
-
+            _desertTexture = Content.Load<Texture2D>("Textures\\Desert");
 
             // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -548,14 +397,6 @@ namespace SpeedCanyon
                 {
                     bullet.Update(gameTime);
 
-                    BoundingSphere bs = _baseModel.Meshes[1].BoundingSphere.Transform(Matrix.CreateScale(5) * Matrix.CreateTranslation(0.0f, 0.52f, -4.0f));
-
-                    if (bs.Contains(bullet.Position) == ContainmentType.Contains)
-                    {
-                        _rotatingClockwise = !_rotatingClockwise;
-                        bullet.IsDead = true;
-                    }
-
 
                     if (_tank2.Collides(bullet.Position))
                     {
@@ -583,118 +424,11 @@ namespace SpeedCanyon
                 }
 
 
-                // Update jet position
-                UpdateKeyframeAnimation(gameTime);
-
-
                 Mouse.SetPosition(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
             }
 
 
             _fadeBox.Update(gameTime);
-        }
-
-
-        void DrawWindmill(Model model, int modelNum, GameTime gameTime)
-        {
-            // CODE CHANGE: Removed culling instructions to avoid unwanted clipping 
-            //              model when converting from MilkShape .fbx to Blender .fbx.
-
-            //graphics.GraphicsDevice.RenderState.CullMode // don't draw backface
-            //         = CullMode.CullClockwiseFace;       // when many vertices
-
-            foreach (ModelMesh mesh in model.Meshes)
-            {
-                // 1: declare matrices
-                Matrix world, scale, rotationZ, translation;
-
-                // 2: initialize matrices
-                scale = Matrix.CreateScale(5.0f, 5.0f, 5.0f);
-                translation = Matrix.CreateTranslation(0.0f, 0.52f, -4.0f);
-                rotationZ = Matrix.CreateRotationY(0.0f);
-
-                if (modelNum == WINDMILL_FAN)
-                {
-                    translation = Matrix.CreateTranslation(0.0f, 0.7f, -4.0f);
-                    // calculate time between frames for system independent speed
-                    _fanRotation += (float)gameTime.ElapsedGameTime.TotalSeconds * 10;
-
-                    // prevent var overflow - store remainder
-                    _fanRotation = _fanRotation % (2.0f * (float)Math.PI);
-
-                    float rot = _fanRotation;
-                    if (_rotatingClockwise)
-                        rot = -rot;
-
-                    rotationZ = Matrix.CreateRotationY(rot);
-                }
-
-                // 3: build cumulative world matrix using I.S.R.O.T. sequence
-                // identity, scale, rotate, orbit(translate&rotate), translate
-                world = scale * rotationZ * translation;
-
-                // 4: set shader parameters
-                foreach (BasicEffect effect in mesh.Effects)
-                {
-                    if (modelNum == WINDMILL_BASE)
-                        effect.World = _baseMatrix[mesh.ParentBone.Index] * world;
-                    if (modelNum == WINDMILL_FAN)
-                        effect.World = _fanMatrix[mesh.ParentBone.Index] * world;
-
-                    effect.View = Camera.View;
-                    effect.Projection = Camera.Projection;
-                    effect.EnableDefaultLighting();
-                }
-                // 5: draw object
-                mesh.Draw();
-
-                //if (modelNum == WINDMILL_BASE)
-                //    BoundingSphereRenderer.Render(mesh.BoundingSphere, Matrix.Identity * world, Camera.View, Camera.Projection);
-            }
-        }
-
-
-        private void DrawCF18(Model model)
-        {
-            // 1: declare matrices
-            Matrix scale, translate, rotateX, rotateY, world;
-
-            // 2: initialize matrices
-            translate = Matrix.CreateTranslation(_currentPosition);
-            scale = Matrix.CreateScale(0.1f, 0.1f, 0.1f);
-            rotateX = Matrix.CreateRotationX(0.0f);
-            rotateY = Matrix.CreateRotationY(_Yrotation);
-
-            // 3: build cumulative world matrix using I.S.R.O.T. sequence
-            // identity, scale, rotate, orbit(translate & rotate), translate
-            world = scale * rotateX * rotateY * translate;
-
-            // set shader parameters
-            foreach (ModelMesh mesh in model.Meshes)
-            {
-                foreach (BasicEffect effect in mesh.Effects)
-                {
-                    effect.World = _jetMatrix[mesh.ParentBone.Index] * world;
-                    effect.View = Camera.View;
-                    effect.Projection = Camera.Projection;
-                    effect.EnableDefaultLighting();
-                    effect.SpecularColor = new Vector3(0.0f, 0.0f, 0.0f);
-
-                    if (KeyFrameNumber() == 1)
-                    {
-                        effect.SpecularColor = new Vector3(1.0f, 0.0f, 0.0f);
-                    }
-                    else if (KeyFrameNumber() == 2)
-                    {
-                        effect.SpecularColor = new Vector3(0.0f, 1.0f, 0.0f);
-                    }
-                    else if (KeyFrameNumber() == 3)
-                    {
-                        effect.SpecularColor = new Vector3(0.0f, 0.0f, 1.0f);
-                    }
-                }
-                mesh.Draw();
-            }
         }
 
 
@@ -710,8 +444,6 @@ namespace SpeedCanyon
             GraphicsDevice.Clear(Color.Black);
             GraphicsDevice.DepthStencilState = DepthStencilState.Default;
 
-            DrawCF18(_jetModel);
-
             foreach (Bullet bullet in _bullets)
             {
                 bullet.Draw(gameTime);
@@ -719,16 +451,14 @@ namespace SpeedCanyon
 
             base.Draw(gameTime);
 
-            DrawWindmill(_baseModel, WINDMILL_BASE, gameTime);
-            DrawWindmill(_fanModel, WINDMILL_FAN, gameTime);
-
             _be.Projection = Camera.Projection;
             _be.View = Camera.View;
-            _be.Texture = _grassTexture;
+            _be.Texture = _desertTexture;
             _be.TextureEnabled = true;
             _be.EnableDefaultLighting();
-            _be.AmbientLightColor = new Vector3(0.8f, 0.8f, 0.6f);
+            _be.AmbientLightColor = new Vector3(0.3f, 0.3f, 0.3f);
             _be.SpecularColor = new Vector3(0.0f, 0.0f, 0.0f);
+            _be.DiffuseColor = new Vector3(0.6f, 0.6f, 0.6f);
             _be.CurrentTechnique.Passes[0].Apply();
 
             GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
@@ -770,44 +500,19 @@ namespace SpeedCanyon
         }
 
 
-        protected void FireShots(GameTime gameTime)
-        {
-            if (_shotCountdown <= 0)
-            {
-                // Did player press space bar or left mouse button?
-                if (Keyboard.GetState().IsKeyDown(Keys.Space) ||
-                    Mouse.GetState().LeftButton == ButtonState.Pressed)
-                {
-                    Vector3 bulletVelocity = Camera.Target - Camera.Position;
-                    bulletVelocity.Normalize();
-                    bulletVelocity *= 10.0f;
-
-                    Bullet newBullet = new Bullet(this, Camera.Position, bulletVelocity);
-                    newBullet.Initialize();
-                    _bullets.Add(newBullet);
-
-                    // Play shot audio
-                    PlayCue("Shot");
-
-                    // Reset the shot countdown
-                    _shotCountdown = _shotDelay;
-                }
-            }
-            else
-                _shotCountdown -= gameTime.ElapsedGameTime.Milliseconds;
-        }
-
         public void PlayCue(string name)
         {
             if (!_muted)
                 _soundBank.PlayCue(name);
         }
 
+
         public void PlayCue(Cue cue)
         {
             if (!_muted)
                 cue.Play();
         }
+
 
         public void AddBullet(Bullet b)
         {
